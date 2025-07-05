9fbe2f20e0e4ce9ace80f74a89fb61f34bc26e2c
Arnz <devops@arnzwebsite.com>
2025-07-05 14:31:51 +0800

    updated uami name

--- terraform/infrastructure/dev/main.tf
+++ terraform/infrastructure/dev/main.tf
@@ -144,7 +144,7 @@ data "azurerm_client_config" "current" {}
 # }
 
 data "azurerm_user_assigned_identity" "main" {
-  name                = "uami-${var.project}-${var.team}-${var.environment}"
+  name                = "uami-arnz-infra-tfc"
   resource_group_name = azurerm_resource_group.rg.name
 }
 
